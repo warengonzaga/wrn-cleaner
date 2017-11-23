d1dd33009879780046991f2aba8ab85a94d23bbe
Waren Gonzaga <warengonzaga.dev@gmail.com>
2017-11-23 08:51:27 +0800

    Add Disk Cleanup Feature

--- src/wrn-cleaner.bat
+++ src/wrn-cleaner.bat
@@ -6,8 +6,8 @@ goto mainMenu
 :mainMenu
 cls
 echo =========================
-echo MS365 PC Cleaner v0.1
-echo by MS365 Developers 2017
+echo WRN PC Cleaner v0.1
+echo by Waren Gonzaga
 echo =========================
 color c
 echo Tools and Optimization
@@ -26,17 +26,37 @@ cls
 echo Auto Cleaning... [Defragment]
 defrag /C /U /V
 pause>null
-goto mainMenu
+goto aDiskClean
 
 :manualOp
 cls
 echo Manual Cleaning... [Defragment]
 echo Please choose drive to be defrag manually...
 fsutil fsinfo drives
-set/p "drive=> "
-if exist %drive%:/ (
-    defrag %drive%: /X /U /V
-    echo Successfully defrag the drive %drive%:
+set/p "driveDef=> "
+if exist %driveDef%:/ (
+    defrag %driveDef%: /X /U /V
+    echo Successfully defrag the drive %driveDef%:
+    pause>null
+) else (
+    echo The drive does not exist!
+    pause>null
+)
+goto mainMenu
+
+:aDiskClean
+echo Auto Cleaning... [Disk Cleanup]
+cleanmgr /d%driveClean%
+goto mainMenu
+
+:manualDiskClean
+echo Manual Cleaning... [Disk Cleanup]
+echo Please choose drive to be clean manually...
+fsutil fsinfo drives
+set/p "driveClean=> "
+if exist %driveClean%:/ (
+    cleanmgr /d%driveClean%
+    echo Successfully cleanup the drive %driveClean%:
     pause>null
 ) else (
     echo The drive does not exist!
